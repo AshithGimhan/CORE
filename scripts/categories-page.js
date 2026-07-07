f623685f616ec2d423a6fbbab44e5f7472e06299
Gimhan <gimhankolonne@gmail.com>
2026-07-07 12:50:15 +0530

    added color removal functionality and improved color picker UI

--- scripts/categories-page.js
+++ scripts/categories-page.js
@@ -9,7 +9,14 @@ const categoryNameElement = document.querySelector('.js-category-name');
 const transactionDropdownElement = document.querySelector('.js-transaction-dropdown');
 const categoryErrorMsgElement = document.querySelector('.js-category-error-msg');
 const typeErrorMsgElement = document.querySelector('.js-type-error-msg');
-const colorPickerElement = document.querySelector('.js-color-picker');
+const colorPickerOpenBtn = document.getElementById('openBtn');
+const colorPickerCloseBtn = document.getElementById('closeBtn');
+const popUpElement = document.querySelector('.js-popup')
+const swatches = document.querySelectorAll('.swatch')
+const swatchConfirmBtn = document.querySelector('.js-swatch-confirm-btn')
+const colorChangeElemet = document.querySelector('.color-picker-container')
+const removeColorBtn = document.querySelector('.js-remove-color')
+
 
 //EVENT LISTENERS
 addCategoryBtn.addEventListener('click', e => {
@@ -18,12 +25,73 @@ addCategoryBtn.addEventListener('click', e => {
 
 })
 
+colorPickerOpenBtn.addEventListener('click', e => {
+    popUpElement.classList.add('visible')
+});
+
+colorPickerCloseBtn.addEventListener('click', e => {
+    popUpElement.classList.remove('visible')
+
+    swatches.forEach(btn => {
+        btn.classList.remove('active');
+    });
+});
+
+swatches.forEach(swatch => {
+    swatch.style.backgroundColor = swatch.dataset.color
+});
+
+swatches.forEach(swatch => {
+    swatch.addEventListener('click', () => {
+        swatchColor = swatch.dataset.color
+
+        swatches.forEach(btn => {
+            btn.classList.remove('active');
+        });
+
+        swatch.classList.add('active');
+        colorChangeElemet.style.color = swatchColor
+
+        confirmBtnChange(swatchColor)
+    });
+})
+
+swatchConfirmBtn.addEventListener('click', event => {
+
+    popUpElement.classList.remove('visible')
+    removeColorBtn.classList.add('remove-color-visible')
+
+})
+
+removeColorBtn.addEventListener('click', event => {
+    swatchColor = '#000000'
+    colorChangeElemet.style.color = ''
+
+    swatches.forEach(btn => {
+        btn.classList.remove('active');
+    });
+    removeColorBtn.classList.remove('remove-color-visible')
+
+});
+
+function confirmBtnChange(swatchColor) {
+    swatchConfirmBtn.addEventListener('mouseover', e => {
+        const datasetColor = swatchColor
+
+        e.target.style.backgroundColor = datasetColor;
+    })
+
+    swatchConfirmBtn.addEventListener('mouseout', e => {
+        e.target.style.backgroundColor = '#fff';
+    })
+
+}
+
 
 //DATA
 const transactions = getTransactions();
 let categoriesList = JSON.parse(localStorage.getItem('categories'))
-
-
+let swatchColor = '#000000';
 
 //INITIALIZE
 activeNavLinks();
@@ -43,14 +111,13 @@ function getCategoryData() {
         category: categoryNameElement.value.toLowerCase(),
         type: transactionDropdownElement.value,
         transactions: getTransactionCountByCategory(categoryName),
-        color: colorPickerElement.value
+        color: swatchColor
     }
 }
 
 function clearInputs() {
     categoryNameElement.value = '';
     transactionDropdownElement.value = 'default';
-    colorPickerElement.value = '#000000';
 }
 
 function getTransactionCountByCategory(category) {
@@ -64,7 +131,6 @@ function getTransactionCountByCategory(category) {
 
     return count
 }
-
 
 function handleAddCategories() {
     const categories = getCategoryData();
@@ -93,6 +159,16 @@ function handleValidation(categories) {
     if (categories.type === 'default') {
         errors.type = 'Transaction type is required'
     }
+
+    if(categories.category === 'food') {
+        errors.category = 'Category already exists'
+    }
+
+    categoriesList.forEach(category => {
+        if (categoriesList.category === categories.category) {
+            errors.category = 'Category already exists'
+        }
+    })
 
     return errors
 }
